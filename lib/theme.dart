6c0b67275d5a1ad128945650b58f8a46b4186bfc
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-01 12:49:37 +0700

    feat: add theme colors and text styles

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffFFFFFF);
+Color greyColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: purpleColor);
