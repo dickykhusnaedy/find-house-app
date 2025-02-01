a15d85803a45913e62ee2668cd09e03588ad0511
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-01 13:08:50 +0700

    feat: define color palette and text styles for theming

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Color
 Color purpleColor = Color(0xff5843BE);
 Color orangeColor = Color(0xffFF9376);
 Color blackColor = Color(0xff000000);
 Color whiteColor = Color(0xffFFFFFF);
 Color greyColor = Color(0xff82868E);
 
+double edge = 24;
+
+// Color button
 TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
 TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
 TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
 TextStyle purpleTextStyle = GoogleFonts.poppins(color: purpleColor);
+
+// Font weigt
+FontWeight fontBold = FontWeight.w900;
+FontWeight fontSemibold = FontWeight.w700;
+FontWeight fontMedium = FontWeight.w600;
+FontWeight fontNormal = FontWeight.w400;
+FontWeight fontLight = FontWeight.w300;
