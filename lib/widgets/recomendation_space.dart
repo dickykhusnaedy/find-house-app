bfb154ae3fb5c6d7676b23cdc83699e9a5d5ec47
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-03 10:19:51 +0700

    feat: make RecomendationSpace widget tappable to navigate to DetailPage

--- lib/widgets/recomendation_space.dart
+++ lib/widgets/recomendation_space.dart
@@ -1,3 +1,4 @@
+import 'package:find_house_app/pages/detail_page.dart';
 import 'package:find_house_app/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -20,81 +21,88 @@ class RecomendationSpace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: SizedBox(
-            width: 130,
-            height: 110,
-            child: Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Image.asset(
-                  imagePath,
-                  width: 130,
-                  height: 110,
-                  fit: BoxFit.cover,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: BoxDecoration(
-                        color: purpleColor,
-                        borderRadius:
-                            BorderRadius.only(bottomLeft: Radius.circular(30))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset('assets/icon_star.png',
-                            width: 18, height: 18),
-                        const SizedBox(width: 2),
-                        Text(
-                          '$rate/5',
-                          style: whiteTextStyle.copyWith(fontSize: 12),
-                        )
-                      ],
-                    ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return DetailPage();
+        }));
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: SizedBox(
+              width: 130,
+              height: 110,
+              child: Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Image.asset(
+                    imagePath,
+                    width: 130,
+                    height: 110,
+                    fit: BoxFit.cover,
                   ),
-                )
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style:
-                  blackTextStyle.copyWith(fontSize: 18, fontWeight: fontBold),
-            ),
-            const SizedBox(height: 2),
-            Text.rich(
-              TextSpan(
-                text: '\$$price ',
-                style: purpleTextStyle.copyWith(
-                    fontSize: 16, fontWeight: fontBold),
-                children: <InlineSpan>[
-                  TextSpan(
-                      text: '/ month',
-                      style: greyTextStyle.copyWith(
-                          fontSize: 14, fontWeight: fontLight)),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          color: purpleColor,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(30))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/icon_star.png',
+                              width: 18, height: 18),
+                          const SizedBox(width: 2),
+                          Text(
+                            '$rate/5',
+                            style: whiteTextStyle.copyWith(fontSize: 12),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
-            const SizedBox(height: 16),
-            Text(
-              location,
-              style:
-                  greyTextStyle.copyWith(fontSize: 14, fontWeight: fontLight),
-            ),
-          ],
-        )
-      ],
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style:
+                    blackTextStyle.copyWith(fontSize: 18, fontWeight: fontBold),
+              ),
+              const SizedBox(height: 2),
+              Text.rich(
+                TextSpan(
+                  text: '\$$price ',
+                  style: purpleTextStyle.copyWith(
+                      fontSize: 16, fontWeight: fontBold),
+                  children: <InlineSpan>[
+                    TextSpan(
+                        text: '/ month',
+                        style: greyTextStyle.copyWith(
+                            fontSize: 14, fontWeight: fontLight)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                location,
+                style:
+                    greyTextStyle.copyWith(fontSize: 14, fontWeight: fontLight),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
