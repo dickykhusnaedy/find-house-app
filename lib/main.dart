b8c45b35f35f85140f22399e2d4ce7e117cf0aef
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-04 10:29:51 +0700

    feat: integrate MultiProvider for SpaceProvider in main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:find_house_app/pages/splash_page.dart';
+import 'package:find_house_app/providers/space_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => SpaceProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          useMaterial3: true,
+        ),
+        home: const SplashPage(),
       ),
-      home: const SplashPage(),
     );
   }
 }
