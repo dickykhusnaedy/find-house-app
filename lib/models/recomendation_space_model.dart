81c3b369a5bb11f1a44f8fb2918f67ee72260101
Dicky Khusnaedy <dicky.khusnaedy.dev@gmail.com>
2025-02-04 10:29:32 +0700

    feat: update RecomendationSpaceModel to support optional fields and JSON serialization

--- lib/models/recomendation_space_model.dart
+++ lib/models/recomendation_space_model.dart
@@ -1,45 +1,50 @@
 class RecomendationSpaceModel {
-  int id;
-  String imagePath;
-  String title;
-  int price;
-  String location;
-  int rate;
+  int? id;
+  String? name;
+  String? city;
+  String? country;
+  int? price;
+  String? imageUrl;
+  int? rating;
+  String? address;
+  String? phone;
+  String? mapUrl;
+  List? photos;
+  int? numberOfKitchens;
+  int? numberOfBedrooms;
+  int? numberOfCupboards;
 
   RecomendationSpaceModel({
-    required this.id,
-    required this.imagePath,
-    required this.title,
-    required this.price,
-    required this.location,
-    required this.rate,
+    this.id,
+    this.name,
+    this.city,
+    this.country,
+    this.price,
+    this.imageUrl,
+    this.rating,
+    this.address,
+    this.phone,
+    this.mapUrl,
+    this.photos,
+    this.numberOfKitchens,
+    this.numberOfBedrooms,
+    this.numberOfCupboards,
   });
 
-  static List<RecomendationSpaceModel> getRecomendation() {
-    List<RecomendationSpaceModel> recomendation = [];
-
-    recomendation.add(RecomendationSpaceModel(
-        id: 1,
-        imagePath: 'assets/space1.png',
-        title: 'Kuretakeso Hott',
-        price: 52,
-        location: 'Bandung, Indonesia',
-        rate: 4));
-    recomendation.add(RecomendationSpaceModel(
-        id: 2,
-        imagePath: 'assets/space2.png',
-        title: 'Roemah Nenek',
-        price: 11,
-        location: 'Seattle, United States',
-        rate: 5));
-    recomendation.add(RecomendationSpaceModel(
-        id: 3,
-        imagePath: 'assets/space3.png',
-        title: 'Darrling How',
-        price: 20,
-        location: 'Jakarta, Indonesia',
-        rate: 3));
-
-    return recomendation;
+  RecomendationSpaceModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    city = json['city'];
+    country = json['country'];
+    price = json['price'];
+    imageUrl = json['image_url'];
+    rating = json['rating'];
+    address = json['address'];
+    phone = json['phone'];
+    mapUrl = json['map_url'];
+    photos = json['photos'];
+    numberOfKitchens = json['number_of_kitchens'];
+    numberOfBedrooms = json['number_of_bedrooms'];
+    numberOfCupboards = json['number_of_cupboards'];
   }
 }
